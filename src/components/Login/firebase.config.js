97bf59ea8d07b84719a16a86389814b9defb23e2
Rifat-Shadman <r.shadman.j@google.com>
2021-03-20 11:09:40 +0600

    template

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAmbOxVYxeE-jrJUPj4yFcMsv0t4dYiOoI",
+    authDomain: "burj-al-arab-zodgin.firebaseapp.com",
+    projectId: "burj-al-arab-zodgin",
+    storageBucket: "burj-al-arab-zodgin.appspot.com",
+    messagingSenderId: "862882850284",
+    appId: "1:862882850284:web:7f3dedfc0c9dd79abc0cc9"
+  };
+
+  export default firebaseConfig ;
